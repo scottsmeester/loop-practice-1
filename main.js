88b0ff089750dafacdad89cae47051c1fccdd481
scottsmeester <scott@smeester.com>
2015-01-19 12:03:25 -0700

    loops practice

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+var i;
+var j;
+
+for (i = 0; i < 600; i+= 100) {
+	console.log (i);
+}
+
+for (i = 1; i <= 64; i*=2) {
+	console.log(i);
+}
+
+for (i = 1; i <= 3; i++) {
+	for (j = 0; j < 3; j++) {
+		console.log(i);
+	}
+}
+
+for (i = 0; i <= 5; i++) {
+	console.log(i * 2);
+}
+
+for (i = 1; i < 6; i++) {
+	console.log(i * 3);
+}
+
+for (i = 9; i >= 0; i--) {
+	console.log(i);
+}
+
+for (i = 0; i < 3; i++) {
+	for (j = 0; j < 4; j++) {
+		console.log(j);
+	}
+}
